0516e4e8f424d9146e72efa9ab1b0a60cf1b7f83
rmdm <rmdm@users.noreply.github.com>
2014-07-13 11:00:15 +0400

    Thesaurus is required

--- test/nlidb.spec.js
+++ test/nlidb.spec.js
@@ -1,4 +1,9 @@
-var nlidb = require('../lib/nlidb')();
+var thesaurus = {
+  get: function (word) {
+  return ['a', 'b'];
+  }
+};
+var nlidb = require('../lib/nlidb')(thesaurus);
 
 describe('NLIDB', function () {
   
@@ -26,6 +31,36 @@ describe('NLIDB', function () {
     expect(res.value[1].value.length).toBe(2);
     expect(res.value[1].value[0].value).toBe('three');
     expect(res.value[1].value[1].value).toBe('words');    
+  });
+  
+  it('has function semanity, used to extend parse tree leaves with semantic information', function () {
+    var tree = {
+      value: [
+        {isLeaf: true, value: 'a'},
+        {isLeaf: true, value: 'b'}
+      ]
+    };
+    var res = nlidb.semantify(tree);
+    expect(res.value[0].occurs[0]).toBe('a');
+    expect(res.value[0].occurs[1]).toBe('b');
+    expect(res.value[1].occurs[0]).toBe('a');
+    expect(res.value[1].occurs[1]).toBe('b');
+  });
+  
+  it('has function onLeaf, used to make some action when leaf is encountered', function () {
+    var tree = {
+      value: {
+        value: [
+          {
+            value: {isLeaf: true, value: 'a'}
+          },
+          {isLeaf: true, value: 'b'}
+        ]
+      }
+    };
+    nlidb.onLeaf(tree, function (leaf) {
+      expect(leaf.value === 'a' || leaf.value === 'b').toBe(true);
+    });
   });
   
 });
