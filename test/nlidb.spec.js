a61909cfc0b19e8bc6f9cc841eb207e8bfcfb74c
d <d@debian>
2014-10-25 13:50:24 +0400

    Fix typo

--- test/nlidb.spec.js
+++ test/nlidb.spec.js
@@ -10,23 +10,27 @@ describe('NLIDB', function () {
   
   it('has function treefy, used to make tree off a query string', function () {
     var str = '';
-    var res = nlidb.treefy(str);
+    var alt = '';
+    var res = nlidb.treefy(str, alt);
     expect(Object.keys(res).length).toBe(3);
     
     var str = 'OneWord';
-    var res = nlidb.treefy(str);
+    var alt = 'oneword';
+    var res = nlidb.treefy(str, alt);
     expect(Object.keys(res).length).toBe(3);
     expect(res.value).toBe('OneWord');
     
     var str = 'Two words';
-    var res = nlidb.treefy(str);
+    var alt = 'two words'
+    var res = nlidb.treefy(str, alt);
     expect(Object.keys(res).length).toBe(1);
     expect(res.value.length).toBe(2);
     expect(res.value[0].value).toBe('Two');
     expect(res.value[1].value).toBe('words');
     
     var str = 'And three words';
-    var res = nlidb.treefy(str);
+    var alt = 'and three words';
+    var res = nlidb.treefy(str, alt);
     expect(Object.keys(res).length).toBe(1);  
     expect(res.value[0].value).toBe('And');
     expect(res.value[1].value.length).toBe(2);
